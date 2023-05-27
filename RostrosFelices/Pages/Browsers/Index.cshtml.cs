c6b47a07210d5bb116857ef924710afd2da7efb5
DAVID ALEJANDRO NUNEZ VELASCO <dnunez1482@campuscecep.edu.co>
2023-05-27 04:48:35 -0500

    Browsers

---
+++ RostrosFelices/Pages/Browsers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RostrosFelices.Data;
+using RostrosFelices.Models;
+
+namespace RostrosFelices.Pages.Browsers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SumpermarketContext _dbContext;
+
+        public IndexModel(SumpermarketContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Servicio> Servicios { get; set; }
+        public async Task OnGet()
+        {
+            Servicios = await _dbContext.Servicios
+                .Include(s => s.Cliente)
+                .Include(s => s.Empleado)
+                .ToListAsync();
+        }
+
+       
+    }
+}
